330544f32c5973d5aa8d591d7349eac3b3baea81
KaiserSov <sara310105@gmail.com>
2022-02-25 14:04:58 -0500

    Realicé crud en back

---
+++ proyecto/src/test/java/com/project/sofka/proyecto/controllers/TareaController.java
@@ -0,0 +1,49 @@
+package com.project.sofka.proyecto.controllers;
+
+import com.project.sofka.proyecto.model.Tarea;
+import com.project.sofka.proyecto.repo.TareaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping
+@Controller("/Tareas")
+public class TareaController {
+
+    @Autowired
+    private TareaRepository tareaRepository;
+
+    @GetMapping("")
+    List<Tarea> index() {
+        return tareaRepository.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    Tarea create(@RequestBody Tarea tarea){
+        return tareaRepository.save(tarea);
+    }
+
+    @PutMapping("{id")
+    Tarea update(@PathVariable String id, @RequestBody Tarea tarea){
+        Tarea tareaFromDb = tareaRepository
+                .findById(id)
+                .orElseThrow(RuntimeException::new);
+
+        tareaFromDb.setNombre(tarea.getNombre());
+        tareaFromDb.setCompletado(tarea.isCompletado());
+
+        return tareaRepository.save(tareaFromDb);
+    }
+
+    @DeleteMapping("{id}")
+    void delete(@PathVariable String id) {
+        Tarea tarea = tareaRepository
+                .findById(id)
+                .orElseThrow(RuntimeException::new);
+        tareaRepository.delete(tarea);
+    }
+}
