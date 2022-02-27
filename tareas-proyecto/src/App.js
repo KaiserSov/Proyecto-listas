8113dfe8b90ed6e993f550ea4df200962f9f8ef0
KaiserSov <sara310105@gmail.com>
2022-02-27 16:03:26 -0500

    Puse íconos y css al "form" de tareas

--- tareas-proyecto/src/App.js
+++ tareas-proyecto/src/App.js
@@ -1,6 +1,29 @@
-
+import { useEffect, useState } from "react";
+import { Container, Col, Row } from "reactstrap";
+import ListaTareas from "./Components/ListaTareas";
+import axios from 'axios';
 
 function App() {
+    const [tareas, setTareas] = useState([]);
+
+    const cargarTareas = () =>{
+        axios.get('http://localhost:8080/tareas')
+        .then(({data}) => setTareas(data))
+    }
+
+    useEffect(cargarTareas, []);
+
+    return (
+        <>
+        <Container>
+            <Row>
+                <Col>
+                  <ListaTareas tareas={tareas} />
+                </Col>
+            </Row>
+        </Container>
+        </>
+    );
 }
 
 export default App;
